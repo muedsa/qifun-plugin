7e839b45a0541cb1fea76adfc6464247205f5328
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-11-22 18:06:44 +0800

    release: 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.muedsa.tvbox.qifun"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
     }
 
     signingConfigs {
